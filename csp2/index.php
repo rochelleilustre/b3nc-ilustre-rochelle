15065eefe637125bf5d95dad1609143c4a71fe46
rochelle ilustre <chellie.ilustre@gmail.com>
2018-02-19 13:18:51 -0800

    csp2 created

---
+++ csp2/index.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'partials/head.php';
+
+?>
+
+
+
+</head>
+<body>
+
+	<h1>INDEX PAGE</h1>
+
+
+	
+
+	<!-- main footer -->
+	<?php include 'partials/main_footer.php'; ?>
+
+
+	<!-- foot -->
+	<?php include 'partials/foot.php'; ?>
+
+</body>
+</html>
---
+++ csp2/partials/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<!-- meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	<meta http-equiv="X-UA-compatible" content="IE=edge">
+
+	<title>SHALLOW performance apparell</title>
+
+	<!-- fav icon -->
+	<link rel="shortcut icon" type="image/x-icon" href="apple-icon-114x114.png">
+
+	<!-- imports bootstrap css -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+
+	<!-- imports custom stylesheet -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
