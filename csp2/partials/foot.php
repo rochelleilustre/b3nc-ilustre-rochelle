f8df4111082b02c64b26e85883b9be506dfda572
rochelle ilustre <chellie.ilustre@gmail.com>
2018-02-20 21:35:22 +0800

    csp2 db connected

---
+++ csp2/partials/foot.php
@@ -0,0 +1,8 @@
+<!-- imports jquery library -->
+<script type="text/javascript" src="assets/lib/jquery-3.2.1min.js"></script>
+
+<!-- imports bootstrap js -->
+<script type="text/javascript" src="assets/css/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+<!-- imports custom script -->
+<script type="text/javascript" src="assets/js/script.js"></script>
---
+++ csp2/partials/main_footer.php
@@ -0,0 +1,3 @@
+<footer class="main-footer">
+		<p>Copyright &copy; 2018 A Capstone Project II <br>  by Rochelle Benet Ilustre</p>
+</footer>
