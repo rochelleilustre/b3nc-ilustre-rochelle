8268f2a9f55825692e6490524c817024eca8d1a4
rochelle ilustre <chellie.ilustre@gmail.com>
2018-03-01 13:44:50 -0800

    crete new item and register

---
+++ csp2/assets/createnewitem.php
@@ -0,0 +1,24 @@
+<?php
+
+require '../connect.php';
+
+$product_name = $_POST['product_name'];
+$product_price = $_POST['product_price'];
+$product_volume = $_POST['product_volume'];
+$product_cat = $_POST['product_cat'];
+$product_brand = $_POST['product_brand'];
+$product_availability = $_POST['product_availability'];
+$product_packaging = $_POST['product_packaging'];
+$product_image = $_POST['product_image'];
+
+$sql = "INSERT INTO product(product_name, price, volume_id, categories_id, brand_id, stocks_id, packaging_id, image) VALUES ('$product_name', '$product_price', '$product_volume', '$product_cat', '$product_brand', '$product_availability', '$product_packaging', '$product_image')";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result)
+	header('location: ../catalog.php');
+else
+	die ('Error: ' . $sql . '<br>' . mysqli_error($conn));
+
+mysqli_close($conn);
+
---
+++ csp2/assets/registration.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../connect.php';
+
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+$email = $_POST['email'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$address = $_POST['address'];
+$contact = $_POST['contact'];
+$image = $_POST['image'];
+
+$sql = "INSERT INTO customer (username, password, email, first_name, last_name, address, contact, image) VALUES ('$username', '$password', '$email', '$first_name', '$last_name', '$address', '$contact', '$image')";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result)
+	header('location: ../login.php');
+else
+	die ('Error: ' . $sql . '<br>' . mysqli_error($conn));
+
+mysqli_close($conn);
